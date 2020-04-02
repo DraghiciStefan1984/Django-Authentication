63e80122bd823b26937ef93423091d8a3e82528a
draghicistefan <draghicistefan1984@gmail.com>
2020-04-02 18:26:23 +0300

    registration logic

--- authentication/authapp/views.py
+++ authentication/authapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def home(request):
@@ -12,8 +14,32 @@ def login_user(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
+            messages.success(request, ('You have been logged in!'))
             return redirect('home')
         else:
+            messages.success(request, ('Login failed, try again!'))
             return redirect('login')
     else:
         return render(request, 'login.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ('You have been logged out!'))
+    return redirect('home')
+
+
+def register_user(request):
+    if request.method=='POST':
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data['usernname']
+            password=form.cleaned_data['password1']
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            messages.success(request, ('You have been registered!'))
+            return redirect('home')
+    else:
+        form=UserCreationForm()
+    return render(request, 'register.html', {'form':form})
