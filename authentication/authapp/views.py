7dca75375103ccbbd071c65f5bab4b4b1491089c
draghicistefan <draghicistefan1984@gmail.com>
2020-04-02 12:07:44 +0300

    login form start

--- authentication/authapp/views.py
+++ authentication/authapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html', {})
+
+def login_user(request):
+    return render(request, 'login.html', {})
